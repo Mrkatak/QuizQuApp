176bc9ff6933d506d3155e156db1f85ffa67bddd
Zein Nur Ichsan <zeinnur@students.amikom.ac.id>
2024-01-05 10:24:20 +0700

    bottom navbar done

--- app/src/main/java/com/example/quizquapp/MainActivity.kt
+++ app/src/main/java/com/example/quizquapp/MainActivity.kt
@@ -3,12 +3,47 @@ package com.example.quizquapp
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    //private lateinit var binding: ActivityMainBinding
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //binding = ActivityMainBinding.inflate(layoutInflater)
         Thread.sleep(3000)
         installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bottom_note -> {
+                    replaceFragment(NoteFragment())
+                    true
+                }
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+        replaceFragment(HomeFragment())
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
     }
 }
